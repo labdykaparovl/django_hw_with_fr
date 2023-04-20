df69078942197c1702d125a8eedb61cf0f131727
ALexShred <mkali2065@gmail.com>
2023-04-20 13:11:18 +0600

    my part of homework

--- glovo/views.py
+++ glovo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
 
-# Create your views here.
+from .models import Courier
+
+
+class Index(generic.ListView):
+    model = Courier
