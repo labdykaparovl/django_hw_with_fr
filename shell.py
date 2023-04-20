df2d413b76fe1ea5bf5b269a0681bf4202dd14fe
ALexShred <mkali2065@gmail.com>
2023-04-20 13:47:46 +0600

    Add code for creating 3 couriers

---
+++ shell.py
@@ -0,0 +1,7 @@
+from glovo.models import *
+
+c1 = Courier.objects.create(name='al', phone_number=12451245)
+c2 = Courier.objects.create(name='qt', phone_number=45674567)
+c3 = Courier.objects.create(name='zb', phone_number=7890780)
+
+
