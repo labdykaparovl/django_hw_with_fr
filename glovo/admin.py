944bd23c15f77a0ceaf3d506312d7003333b0da5
labdykaparovl <abdykaparov06@icloud.com>
2023-04-20 13:54:18 +0600

    finished 2

--- glovo/admin.py
+++ glovo/admin.py
@@ -5,9 +5,5 @@ from .models import Courier
 admin.site.register(Courier)
 
 
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'quantity', 'courier')
-
-
-admin.site.register(Order, OrderAdmin)
+admin.site.register(Order)
 
